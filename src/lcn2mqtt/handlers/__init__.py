befdd35bc3df1837b929aa9e43971e19d8dcff77
Andre Lengwenus <alengwenus@rosen-group.com>
2026-08-21 11:10:51 +0200

    Add logic_op handlers
    * Add logic_op handlers
    
    * Add tests

--- src/lcn2mqtt/handlers/__init__.py
+++ src/lcn2mqtt/handlers/__init__.py
@@ -1,11 +1,23 @@
 """Peripheral component handlers for the LCN <-> MQTT bridge."""
 
-from . import binsensor, extra, led, motor, output, relay, setpoint, threshold, variable
+from . import (
+    binsensor,
+    extra,
+    led,
+    logic_op,
+    motor,
+    output,
+    relay,
+    setpoint,
+    threshold,
+    variable,
+)
 
 __all__ = [
     "binsensor",
     "extra",
     "led",
+    "logic_op",
     "motor",
     "output",
     "relay",
